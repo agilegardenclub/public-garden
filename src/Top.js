87a500939624c2e5f7c37c210a5479185b995f6e
Philip Johnson <johnson@hawaii.edu>
2022-03-03 07:54:50 -1000

    update navbar

--- src/Top.js
+++ src/Top.js
@@ -2,22 +2,29 @@ import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
-import { greenColorCode } from './Constants';
 
 /**
  * Display the "NavBar" at the top of the page.
  */
 function Top() {
-  const navbarStyle = { backgroundColor: greenColorCode, color: 'white', fontWeight: 'bold' };
   return (
     <div>
-      <Navbar style={navbarStyle}>
-        <Container>Agile Garden Club: Public Garden View</Container>
-        <Container className="justify-content-center">
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img
+              alt=""
+              src="https://agilegardenclub.com/img/peony.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+            />{' '}
+            Agile Garden Club
+          </Navbar.Brand>
           <Nav>
-            <Nav.Item><Nav.Link href="#overview" style={navbarStyle}>Overview</Nav.Link></Nav.Item>
-            <Nav.Item><Nav.Link href="#plan" style={navbarStyle}>Plan</Nav.Link></Nav.Item>
-            <Nav.Item><Nav.Link href="#seeds" style={navbarStyle}>Seeds</Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link href="#overview">Overview</Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link href="#plan">Plan</Nav.Link></Nav.Item>
+            <Nav.Item><Nav.Link href="#seeds">Seeds</Nav.Link></Nav.Item>
           </Nav>
         </Container>
       </Navbar>

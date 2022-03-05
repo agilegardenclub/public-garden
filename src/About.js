18c2bffaff3a893b9657d6df1e2bf8a8648244f0
Philip Johnson <johnson@hawaii.edu>
2022-03-04 15:41:44 -1000

    checkpoint

--- src/About.js
+++ src/About.js
@@ -10,9 +10,10 @@ function About() {
     <div style={{ paddingTop: '1em' }}>
       <Container>
         <Alert variant="success">
-          <Alert.Heading>About the public garden view</Alert.Heading>
-          <p>Welcome to the Agile Garden Club (AGC) public garden view.</p>
-          <p>The owner of a garden representation in AGC has the option to make the garden &quot;public&quot;, which results in the system generating a publicly accessable web page with read-only information about the garden.</p>
+          <Alert.Heading>About Agile Garden Club and the Public Garden View</Alert.Heading>
+          <p>Welcome to this example of an Agile Garden Club public garden view.</p>
+          <p><b>Agile Garden Club</b> is an organization providing technology for collaborative planning and management of home gardens. We are developing mobile-first applications to support not just garden planning, but also to support efficient and effective sharing of data and insights amongst gardeners in the same geographic region. Our support for collaboration and &quot;local communities of practice&quot; sets AGC apart from other garden planning systems.</p>
+          <p>The <b>Public Garden View</b> is an option available to owners of a garden represented in AGC. Enabling this view tells the system to generate a web page like this one. The gardeners can share its unique URL with others to provide details about their garden without them having to download our app.</p>
         </Alert>
       </Container>
     </div>

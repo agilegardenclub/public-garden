81931ebf16ec0b1147cbfe9979ee70198aa42aa2
Philip Johnson <johnson@hawaii.edu>
2022-03-10 11:16:49 -1000

    Data model done for now.

--- src/sections/Overview.js
+++ src/sections/Overview.js
@@ -16,11 +16,11 @@ function Overview() {
         <Documentation>
           <p>To provide gardeners with locally relevant information, each AGC garden is assigned to a single <b>Chapter</b>. Chapters are defined by AGC administrators, and consist of one or more continguous Zip Codes. Insights about seeds, plants, pests, weather, outcomes, and so forth are shared between gardens in the same chapter. </p>
         </Documentation>
-        <Row>
-          <Col md className="pb-3">
+        <Row md={1} lg={2} className="g-4">
+          <Col>
             <GardenOverviewCard/>
           </Col>
-          <Col md>
+          <Col>
             <ChapterOverviewCard/>
           </Col>
         </Row>

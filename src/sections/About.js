b2e1de6a14c764fddd8f6fb182ffb7ccdc66f6aa
Philip Johnson <johnson@hawaii.edu>
2022-03-11 16:19:42 -1000

    wordsmith

--- src/sections/About.js
+++ src/sections/About.js
@@ -11,12 +11,12 @@ function About() {
     <div>
       <Documentation>
         <p>Welcome to a sample Agile Garden Club public garden view.</p>
-        <p><b>Agile Garden Club</b> is an organization providing technology for collaborative planning and management of home gardens. We are designing mobile-first applications to support not just individual garden planning and management, but also
+        <p><b>Agile Garden Club</b> is an organization providing technology for collaborative planning and management of home gardens. We are designing technology to support not just individual garden planning and management, but also
           efficient and effective sharing of data and insights amongst gardeners in the same geographic region.</p>
         <p>AGC differs from other garden planning technologies in two important ways:</p>
         <ol>
           <li>Superior support for collaboration, for historical insights over multiple seasons, and for building &quot;local communities of practice&quot;.</li>
-          <li>Unlike many other garden planning tools, AGC is not sponsored by a seed company. We are committed to locally produced seeds, and all seed data comes from members.</li>
+          <li>Unlike many other garden planning tools, AGC accepts no funding from seed companies. All seed information is crowd-sourced by chapter members based upon real-world experience in local conditions. AGC supports the use of home-harvested seeds and seed sharing whenever possible.</li>
         </ol>
         <p>The <b>Public Garden View</b> is an option available to owner(s) of an AGC garden. Enabling this view tells the system to generate a web page like this one. Garden owners can share the web page to provide details about their garden
           without requiring the AGC app. This Public Garden View does not reveal the identity of gardeners or the specific location of the garden. </p>

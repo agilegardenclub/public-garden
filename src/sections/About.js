b1902d7b0adbc30d583d9334a526ac79e055f8e2
Philip Johnson <johnson@hawaii.edu>
2022-03-13 18:50:39 -1000

    Update about

--- src/sections/About.js
+++ src/sections/About.js
@@ -12,10 +12,11 @@ function About() {
       <Documentation>
         <p>Welcome to an example Agile Garden Club public garden view.</p>
         <p><b>Agile Garden Club</b> is an organization whose mission is to help build local &quot;communities of practice&quot; that result in better planning and management of home gardens. To accomplish this, we are designing technologies to help gardeners help each other through shared experiences and insights. </p>
-        <p>AGC technologies differ from other garden technologies in two primary ways:</p>
+        <p>AGC technologies differ from other garden technologies in several ways:</p>
         <ol>
-          <li>Superior support for collaboration, for historical insights over multiple seasons, and for local community building.</li>
-          <li>Unlike many other garden technology organizations, AGC is not funded by a seed company. Instead, AGC implements &quot;crowd sourcing&quot; of seed information by chapter members based upon real-world experience in local conditions. We want to facilitate and increase the use of home-harvested seeds and seed sharing whenever possible.</li>
+          <li>Many garden technologies represent gardening as a solitary, single season activity. In contrast, AGC views gardening as a collaborative activity, with support for gaining insights over multiple seasons by all of a member&lsquo;s local gardening community.</li>
+          <li>Many garden technologies are sponsored in whole or in part by a seed vendor. In contrast, AGC is vendor-independent. AGC seed data does not come directly from a vendor, but instead is incrementally built by chapter members based upon their real-world experience in local conditions. Vendor-independence frees us to support and spread the use of home-harvested seeds and seed sharing whenever possible.</li>
+          <li>AGC data is Chapter-specific. If you live in Alaska, garden data from Florida is of limited use. The AGC community is divided into local Chapters covering a small geographic region. This helps ensure the relevance of community-supplied data on plant and seed outcomes.</li>
         </ol>
         <p>This <b>Public Garden View</b> is an option available to AGC Chapter members. Enabling this view tells our technology to generate a web page like this one. AGC Chapter members can share the web page address to provide details about their garden to others regardless of whether they are a Chapter member or not. This Public Garden View is designed to not reveal the identity of gardeners or the specific location of the garden. </p>
       </Documentation>

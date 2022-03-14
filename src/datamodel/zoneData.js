9779135bc190e7d77787f2f75fc95d3f9f1fee26
Philip Johnson <johnson@hawaii.edu>
2022-03-13 17:03:08 -1000

    updates for circulation

--- src/datamodel/zoneData.js
+++ src/datamodel/zoneData.js
@@ -1,18 +1,5 @@
 /* Raw Data to define climate zones. */
 
-// See https://en.wikipedia.org/wiki/Hardiness_zone for a critique of hardiness zones. They do not account for:
-// * Frost dates
-// * Frequency and/or reliability of snow cover (can provide insulation)
-// * Number of cold nights per year
-// * Summer temperature
-// * Insolation
-// * Day length (required for flowering)
-// * Vernalization (duration of low temperature)
-// * Humidity, Precipitation, Storms, Rainy-Dry cycles, soil type, soil drainage, water retention.
-
-// Might also want to consider Trewartha climate types:
-// https://www.reddit.com/r/MapPorn/comments/7yt1pj/trewartha_climate_types_of_washington_state_1260/
-
 export const hardinessZoneData = [
   {
     id: 'zone-8a',

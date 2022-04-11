68b73b5a4c1b8302b7418bc94d59590b808227ba
Philip Johnson <johnson@hawaii.edu>
2022-04-11 09:38:50 -1000

    Update chapter overview

--- src/components/ChapterOverviewCard.js
+++ src/components/ChapterOverviewCard.js
@@ -4,41 +4,42 @@ import { Field } from './Field';
 import { chapterOverview } from '../datamodel/ChapterOverview';
 import { ZoneBadge } from './ZoneBadge';
 import { PictureCarousel } from './PictureCarousel';
-import { PlantRatingList } from './PlantRatingList';
-import { PlantPopularityList } from './PlantPopularityList';
 
 export function ChapterOverviewCard() {
   const name = chapterOverview.name();
   const pictureData = chapterOverview.pictures();
   const age = chapterOverview.age();
-  const zipCodes = chapterOverview.zipCodes().join(', ');
+  const zipCodes = chapterOverview.zipCodes()
+    .join(', ');
   const members = chapterOverview.members();
   const gardens = chapterOverview.gardens();
-  const zoneBadges = chapterOverview.zoneIDs().map((zoneID, index) => ZoneBadge({ zoneID, index }));
-  const popularPlantRatings = chapterOverview.plantRatings('popular');
-  const yieldPlantRatings = chapterOverview.plantRatings('yield');
-  const localPlantRatings = chapterOverview.plantRatings('local');
+  const zoneBadges = chapterOverview.zoneIDs()
+    .map((zoneID, index) => ZoneBadge({ zoneID, index }));
+  // const popularPlantRatings = chapterOverview.plantRatings('popular');
+  // const yieldPlantRatings = chapterOverview.plantRatings('yield');
+  // const localPlantRatings = chapterOverview.plantRatings('local');
   return (
     <Card>
       <Card.Header><h5>Chapter: {name}</h5></Card.Header>
       <PictureCarousel pictureData={pictureData}/>
       <Card.Body>
         <Stack gap={1}>
-          <Field title='Age:'>
+          <Field title="Age:">
             {age} seasons
           </Field>
-          <Field title='Zip Codes:'>
+          <Field title="Zip Codes:">
             {zipCodes}
           </Field>
-          <Field title='Hardiness Zone(s):'>
+          <Field title="Hardiness Zone(s):">
             {zoneBadges}
           </Field>
-          <Field title='Number of members:'>
+          <Field title="Number of members:">
             {members}
           </Field>
-          <Field title='Number of gardens:'>
+          <Field title="Number of gardens:">
             {gardens}
           </Field>
+          {/*
           <Field title='Top 5 Plants (popularity):' direction='vertical'>
             <PlantPopularityList ratingData={popularPlantRatings}/>
           </Field>
@@ -48,6 +49,7 @@ export function ChapterOverviewCard() {
           <Field title='Top 5 Plants (local seeds):' direction='vertical'>
             <PlantRatingList ratingData={localPlantRatings}/>
           </Field>
+          */}
         </Stack>
       </Card.Body>
     </Card>

c526b8fff22e8845f38dadb6ca50764f410642d6
Philip Johnson <johnson@hawaii.edu>
2022-08-16 11:35:29 -1000

    a little better. checkpoint

--- src/components/GardenOverviewCard.js
+++ src/components/GardenOverviewCard.js
@@ -19,6 +19,7 @@ export function GardenOverviewCard() {
   const pictureData = gardenOverview.pictures();
   const gardenYears = getGardenYears(gardenID);
   const outcomeData = gardenOverview.outcomeData();
+  console.log('outcomeData', outcomeData);
   const size = gardenOverview.currentSize();
   const beds = gardenOverview.currentBeds();
   const totalPlantings = gardenOverview.totalPlantings();
--- src/components/OutcomeByVariety.js
+++ src/components/OutcomeByVariety.js
@@ -3,29 +3,33 @@ import { Col, Container, Row } from 'react-bootstrap';
 import { varietyData } from '../datamodel/data/varietyData';
 import { familyData } from '../datamodel/data/familyData';
 import { PlantingHistory } from '../datamodel/PlantingHistory';
-import { TimelineData } from './TimelineData';
 import { getGardenName } from './GardenName';
 import { getVarietyNameShort } from '../datamodel/VarietyInfo';
 import { NestedDropdown } from './NestedDropdown';
+import { getGardenOutcomeData } from '../datamodel/OutcomeDataInfo';
+import { getGardenID } from '../datamodel/GardenInfo';
+import { OutcomeChart } from './OutcomeChart';
 
 export function OutcomeByVariety() {
   const gardenName = getGardenName();
+  const initialOutcomeData = getGardenOutcomeData(getGardenID(gardenName));
   const plantingHistory = new PlantingHistory({ gardenName, varietyData, familyData });
   const menuItems = plantingHistory.varietyDropdownMenuItems();
   const initialVarietyID = menuItems[0].items[0].eventKey;
+  console.log(initialVarietyID);
   const initialVarietyName = menuItems[0].items[0].label;
   const [selectedVarietyName, setVarietyName] = useState(initialVarietyName);
-  const [historyData, setHistoryData] = useState(plantingHistory.historyData({ varietyID: initialVarietyID }));
+  const [outcomeData, setOutcomeData] = useState(initialOutcomeData);
   const onSelect = (eventKey) => {
     if (eventKey) {
       setVarietyName(getVarietyNameShort(eventKey));
-      setHistoryData(plantingHistory.historyData({ varietyID: eventKey }));
+      setOutcomeData(initialOutcomeData);
     }
   };
   return (
     <Container>
       <Row className="mb-3"><Col xs={2}>Select Variety:</Col> <Col><NestedDropdown title={selectedVarietyName} items={menuItems} onSelect={onSelect}/></Col></Row>
-      { historyData && <TimelineData historyData={historyData}/> }
+      { outcomeData && <OutcomeChart outcomeData={outcomeData} /> }
     </Container>
   );
 }
